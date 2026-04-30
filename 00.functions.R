b5124771e84b3b40b802548bdc395f53538efc85
sfontan <savefontana@gmail.com>
2026-05-01 00:03:26 +0200

    Added modified functions, ready for Table 2 generation

--- 00.functions.R
+++ 00.functions.R
@@ -15,7 +15,7 @@ library(metafor)
 
 
 
-impute_missing_se <- function(data, target_theta_col, target_se_col, n.total = 100) {
+impute_missing_se <- function(data, target_theta_col, target_se_col, n_col ) {
   
   # Identify reported and unreported indices
   rep_idx <- which(!is.na(data[[target_theta_col]]))
@@ -28,10 +28,14 @@ impute_missing_se <- function(data, target_theta_col, target_se_col, n.total = 1
   
   # Calculate k_hat using only the reported studies (Eq. 6)
   precisions <- 1 / (data[[target_se_col]][rep_idx]^2)
-  k_hat <- sum(precisions) / sum(n.total * length(rep_idx))
+  num <- sum(precisions)
+  den <- sum(data[[n_col]][rep_idx])
+
+
+  k_hat <- num / den
   
   # Impute the missing standard errors
-  data[[target_se_col]][unrep_idx] <- sqrt(1 / (k_hat * n.total))
+  data[[target_se_col]][unrep_idx] <- sqrt(1 / (k_hat * data[[n_col]][unrep_idx]))
   
   return(data)
 }
@@ -53,7 +57,7 @@ impute_missing_se <- function(data, target_theta_col, target_se_col, n.total = 1
 # -----------------------------------------
 
 
-# Univariate Imputation under MAR
+# Univariate Imputation under MAR (remember it assumes se already imputed)
 run_univariate_imputation <- function(data, theta_col = "log_RR2", se_col = "se_RR2", m = 1000, new_version = FALSE, method.re = "REML") {
   
   # Find reported and unreported indexes
@@ -73,8 +77,19 @@ run_univariate_imputation <- function(data, theta_col = "log_RR2", se_col = "se_
   rep_idx <- 1:n_rep
   unrep_idx <- (n_rep + 1):K
   
+
+  # if nothing missing, return null
+  if (length(unrep_idx) == 0) return(NULL)
+
+  # but also we need at least 2 reported studies
+  if (length(rep_idx) < 2) {
+    warning("Fewer than 2 reported studies. Cannot estimate heterogeneity. Skipping.")
+    return(NULL) 
+  }
+
+
   # Fit REML on the reported data (Naive Estimate)
-  res_naive <- rma(yi = data_rep[[theta_col]], sei = data_rep[[se_col]], method = method.re)
+  res_naive <- metafor::rma(yi = data_rep[[theta_col]], sei = data_rep[[se_col]], method = method.re)
   theta_MA <- as.numeric(res_naive$beta)
   tau2_hat <- as.numeric(res_naive$tau2)
   SE2_mean <- res_naive$se^2
@@ -84,12 +99,15 @@ run_univariate_imputation <- function(data, theta_col = "log_RR2", se_col = "se_
   J_K <- matrix(1, K, K) 
 
   #Total cov matrix
-  Sigma <- diag(data_ordered[[se_col]]^2) + (SE2_mean * J_K) #paper version
-  
-  if (new_version) Sigma <- diag(data_ordered[[se_col]]^2 + tau2_hat) + (SE2_mean * J_K) 
+
+  if (new_version) {
+    Sigma <- diag(data_ordered[[se_col]]^2 + tau2_hat) + (SE2_mean * J_K) 
+    } else {
+        Sigma <- diag(data_ordered[[se_col]]^2) + (SE2_mean * J_K) # old paper version
+    }
 
 
-  # Partitioning (Eq. 9)
+  # Partitioning (Eq. 10)
   Sigma_RR <- Sigma[rep_idx, rep_idx]
   Sigma_UU <- Sigma[unrep_idx, unrep_idx]
   Sigma_UR <- Sigma[unrep_idx, rep_idx]
@@ -97,13 +115,13 @@ run_univariate_imputation <- function(data, theta_col = "log_RR2", se_col = "se_
   
   # Conditionals (Eq. 11) 
   theta_R <- data_rep[[theta_col]]
-  inv_Sigma_RR <-solve(Sigma_RR) 
+  inv_Sigma_RR <- solve(Sigma_RR) 
 
   mu_cond <- as.numeric(theta_MA + Sigma_UR %*% inv_Sigma_RR %*% (theta_R - theta_MA))
   Sigma_cond <- Sigma_UU - Sigma_UR %*% inv_Sigma_RR %*% Sigma_RU
   
   # Generate M Imputations (we get a matrix m x n_unrep)
-  imputed_draws <- mvrnorm(n = m, mu = mu_cond, Sigma = Sigma_cond)
+  imputed_draws <- MASS::mvrnorm(n = m, mu = mu_cond, Sigma = Sigma_cond)
 
  #probably mvnorm create a vector if we only have one unreported study and can create problems 
   if (n_unrep == 1) {
@@ -121,12 +139,20 @@ run_univariate_imputation <- function(data, theta_col = "log_RR2", se_col = "se_
   ))
 }
 
+
+
+# --------------------------
 #  Importance Sampling 
+# --------------------------
+
 adj_univariate <- function(mi_results, delta = 0.5, select_type = "zscore", method.re = "REML") {
 
   draws        <- mi_results$draws
   data_ordered <- mi_results$data_ordered
   unrep_idx    <- mi_results$unrep_idx
+  
+  # ensure draws is a matrix
+  if (!is.matrix(draws)) draws <- matrix(draws, ncol = length(unrep_idx))
   M            <- nrow(draws)
   
 
@@ -140,20 +166,28 @@ adj_univariate <- function(mi_results, delta = 0.5, select_type = "zscore", meth
   # extract the se useful for the z scores
   sei_unrep <- data_ordered[[se_col]][unrep_idx]   
 
+  # I add a small useless check just to be sure 
+  if (length(sei_unrep) != ncol(draws) ) stop ("Something is wrong in the dimensions of the unreported studies")
+
 if (select_type == "zscore") {
-    # divides every row in 'draws' by the 'sei_unrep' vector
+    # divides every row in 'draws' by the 'sei_unrep' vector to create z score
     z_matrix <- sweep(draws, 2, sei_unrep, FUN = "/")
-    log_weights <- -delta * rowSums(z_matrix)
+    log_weights <- - delta * rowSums(z_matrix)
   } else {
-    log_weights <- -delta * rowSums(draws)
+    log_weights <- - delta * rowSums(draws)
   }
 
   # rma on each complete dataset 
+  # vector of thetas that is like [c(reported), NA, NA,NA ...]
+
+  yi_base <- data_ordered[[theta_col]]
+
   fits <- lapply(seq_len(M), function(m) {
-    d <- data_ordered
-    d[[theta_col]][unrep_idx] <- draws[m, ] #complete df
+    yi_complete <- yi_base
+    yi_complete[unrep_idx] <- draws[m, ] # complete the vector of thetas with the draws from here
     tryCatch(
-      rma(yi = d[[theta_col]], sei = d[[se_col]], method = method.re),
+      rma(yi = yi_complete, sei = data_ordered[[se_col]], method = method.re,
+          control = list(stepadj = 0.1, rel.tol = 1e-8, maxiter = 1000)),
       error = function(e) NULL
     )
   })
@@ -230,10 +264,12 @@ if (select_type == "zscore") {
 
 
 
+dim(t(iris[, c("Sepal.Length", "Sepal.Width")]))
+dim(iris$Sepal.Length)
+names(iris)
 
-
-
-
+mm <- matrix (1:9, 3, 3)
+as.character ( t ( mm[,1:2]) )
 
 
 # ---------------------------
@@ -242,18 +278,31 @@ if (select_type == "zscore") {
 
 run_bivariate_imputation <- function(data, theta_cols, se_cols, rho_w = 0.4, m = 1000, new_version = FALSE, method.re = "REML") {
   
+  if (rho_w <= -1 || rho_w >= 1) {
+    stop("rho_w must be strictly between -1 and 1.")
+  }
+
+
   K <- nrow(data)
   
-  # study-major vector 1x2K
-  y_vec <- as.numeric(t(data[, theta_cols]))
-  se_vec <- as.numeric(t(data[, se_cols]))
+  # study-major vector of length 2K (remember that as.numeric flatten by columns)
+  # so we have (Out1study1, out2study1, ...... out1studyK, out2studyK)
+  y_vec <- as.numeric(t(as.matrix ( data[, theta_cols])) ) 
+  se_vec <- as.numeric(t( as.matrix(data[, se_cols])))
   
   rep_idx <- which(!is.na(y_vec))
   unrep_idx <- which(is.na(y_vec))
   
+
+  
   # if nothing missing, return null
   if (length(unrep_idx) == 0) return(NULL)
 
+  # but also we need at least 2 reported studies
+  if (length(rep_idx) < 2) {
+    warning("Fewer than 2 reported studies. Cannot estimate heterogeneity. Skipping.")
+    return(NULL) 
+  }
 
   # builde the V's (within study cov)
   V_list <- list()
@@ -264,12 +313,12 @@ run_bivariate_imputation <- function(data, theta_cols, se_cols, rho_w = 0.4, m =
     V_list[[i]] <- matrix(c(v1, cov_12, cov_12, v2), 2, 2)
   }
 
-  V_full <- as.matrix(bdiag(V_list))
+  V_full <- as.matrix(Matrix::bdiag(V_list))
   
   # Create Long Data 
   # --> df that has row 1 = study 1 outcome 1, row 2 = study 1 outcome 2, row 3 = study 2 outcome 1 ....
 
-  alternated.vector <- data.frame(
+  alternated.df <- data.frame(
     study_id = rep(data$study_id, each = 2),
     outcome  = factor(rep(c("O1", "O2"), times = K)),
     yi       = y_vec,
@@ -277,23 +326,20 @@ run_bivariate_imputation <- function(data, theta_cols, se_cols, rho_w = 0.4, m =
     obs_id   = 1:(2*K)
   )
   
-  alternated.vector_rep <- alternated.vector[rep_idx, ]
+  alternated.df_rep <- alternated.df[rep_idx, ]
   V_rep <- V_full[rep_idx, rep_idx]
   
   # Fit Bivariate REML  (Eq. 12 & 13)
   res_naive <- rma.mv(yi, V = V_rep, 
                       mods = ~ outcome - 1, 
                       random = ~ outcome | study_id, struct = "UN",  # true effects varies for each study id (heterogeneity)
-                      data = alternated.vector_rep, method = method.re, 
+                      data = alternated.df_rep, method = method.re, 
                       control=list(rel.tol=1e-8, maxiter=1000))
   
   # Construct Total Covariance Matrix Sigma
   Cov_theta_MA <- vcov(res_naive)
   J_K <- matrix(1, K, K)
   
-  #  Eq. 13
-  Sigma <- V_full + kronecker(J_K, Cov_theta_MA)
-
   if (new_version) { 
   tau2_1 <- res_naive$tau2[1]
   tau2_2 <- res_naive$tau2[2]
@@ -306,26 +352,41 @@ run_bivariate_imputation <- function(data, theta_cols, se_cols, rho_w = 0.4, m =
     I_K <- diag(K)
   
   Sigma <- V_full + kronecker(I_K, Psi) + kronecker(J_K, Cov_theta_MA)
+  } else {
+    Sigma <- V_full + kronecker(J_K, Cov_theta_MA)
   }
 
+  #########################################
+  ## NOTE on the new implementation 
+  ## I was thinking that this could work properly when we have a lot of studies to estimate Psi
+  ## But if for example we hvae K = 6, and maybe ORB on two studies, then with only 4 studies we 
+  ## need to estimate the whole Psi. 
+  ## I was thinking as an idea to do if length(rep_idx) < some value, then we estimate Psi using 
+  ## struct = "VC", this way we set the off diagonals of Psi = 0, but maybe its more stable? 
+  # 
+  # --> ths problem applies also in tau on the univariate case ....
+  #########################################
+
   # partitioning 
-  Sigma_RR <- Sigma[rep_idx, rep_idx]
-  Sigma_UU <- Sigma[unrep_idx, unrep_idx]
-  Sigma_UR <- Sigma[unrep_idx, rep_idx]
-  Sigma_RU <- Sigma[rep_idx, unrep_idx]
+  Sigma_RR <- Sigma[rep_idx, rep_idx, drop = FALSE]
+  Sigma_UU <- Sigma[unrep_idx, unrep_idx, drop = FALSE]
+  Sigma_UR <- Sigma[unrep_idx, rep_idx, drop = FALSE]
+  Sigma_RU <- Sigma[rep_idx, unrep_idx, drop = FALSE]
   
   #  Conditionals (Eq. 11)
-  theta_MA_vec <- rep(coef(res_naive), times = K) 
-  theta_R_MA <- theta_MA_vec[rep_idx]
-  theta_U_MA <- theta_MA_vec[unrep_idx]
+  theta_MA_vec <- rep(coef(res_naive), times = K)  # vector 2K
+  theta_R_MA <- theta_MA_vec[rep_idx] # vector K_R
+  theta_U_MA <- theta_MA_vec[unrep_idx] # vector K_U
   theta_R <- y_vec[rep_idx]
+
+
   
   inv_Sigma_RR <- solve(Sigma_RR)
   mu_cond <- theta_U_MA + Sigma_UR %*% inv_Sigma_RR %*% (theta_R - theta_R_MA)
   Sigma_cond <- Sigma_UU - Sigma_UR %*% inv_Sigma_RR %*% Sigma_RU
   
   # Generate M Imputations
-  imputed_draws <- mvrnorm(n = m, mu = as.numeric(mu_cond), Sigma = Sigma_cond)
+  imputed_draws <- MASS::mvrnorm(n = m, mu = as.numeric(mu_cond), Sigma = Sigma_cond)
   if (length(unrep_idx) == 1) {
     imputed_draws <- matrix(imputed_draws, ncol = 1)
   }
@@ -334,7 +395,7 @@ run_bivariate_imputation <- function(data, theta_cols, se_cols, rho_w = 0.4, m =
     draws = imputed_draws,
     unrep_idx = unrep_idx,
     res_naive = res_naive,
-    alternated.vector = alternated.vector,
+    alternated.df = alternated.df,
     V_full = V_full,
     theta_cols = theta_cols
   ))
@@ -345,16 +406,21 @@ run_bivariate_imputation <- function(data, theta_cols, se_cols, rho_w = 0.4, m =
 adj_bivariate <- function(mi_results, delta = 0.5, select_type = "zscore", method.re = "REML") {
 
   draws <- mi_results$draws
-  alternated.vector <- mi_results$alternated.vector
+  alternated.df <- mi_results$alternated.df
   unrep_idx <- mi_results$unrep_idx
   V_full <- mi_results$V_full
+  
+  if (!is.matrix(draws)) draws <- matrix(draws, ncol = length(unrep_idx))
   M <- nrow(draws)
 
   # extract the se useful for the z scores
-  sei_unrep <- alternated.vector$sei[unrep_idx]  
+  sei_unrep <- alternated.df$sei[unrep_idx]  
 
+  # useless check 
+  if (ncol(draws) != length(sei_unrep)) stop ("Something is wrong with the number of unreported studies")
 
-      # NOTE: here the paper is not clear. 
+   
+    # NOTE
     # I decided to have one weight for each imputation m. otherwise we'd have w_j^(m) but this mean two weight for each 
     # imputation in the topiramate case. (this choice doesnt affect simulation study)
 
@@ -369,7 +435,7 @@ adj_bivariate <- function(mi_results, delta = 0.5, select_type = "zscore", metho
 
   # loop for each draw
   fits <- lapply(seq_len(M), function(m) {
-    d <- alternated.vector
+    d <- alternated.df
 
     # create complete dataset
     d$yi[unrep_idx] <- draws[m, ]
@@ -385,11 +451,13 @@ adj_bivariate <- function(mi_results, delta = 0.5, select_type = "zscore", metho
   })
 
   # extract valid
-  valid       <- !sapply(fits, is.null)
+  valid       <- sapply(fits, function(x) !is.null(x))
   fits        <- fits[valid]
   log_weights <- log_weights[valid]
 
-  # Extract 
+  if (length(fits) == 0) stop("All rma.mv fits failed")
+
+  # Extract (note that sapply bind vectors together by columns)
   theta_MA_m <- t(sapply(fits, function(r) as.numeric(r$beta)))  # M x 2
   var_MA_m   <- t(sapply(fits, function(r) diag(r$vb)))           # M x 2
 
@@ -398,20 +466,24 @@ adj_bivariate <- function(mi_results, delta = 0.5, select_type = "zscore", metho
   w_norm <- exp(log_weights - max_log) / sum(exp(log_weights - max_log))
   
   # Adjusted estimates 
-  theta_adj <- colSums(w_norm * theta_MA_m)               # length-2 vector
+  # remember w_norm is a vector of 1000 weights
+  # theta_MA_m is a 1000 x 2 matrix
+  theta_adj <- colSums(w_norm * theta_MA_m)               # this returns a VECTOR of length 2
 
   # Rubin rule
   var_within  <- colSums(w_norm * var_MA_m)
   var_between <- colSums(w_norm * sweep(theta_MA_m, 2, theta_adj, "-")^2)
-  total_var   <- var_within + var_between
+  total_var   <- var_within + var_between #length 2 vector
 
+  if (length(theta_adj) != length(total_var) ) stop ("Something is off in the dimension of outcomes")
   results_df <- data.frame(
     Outcome  = c("O1", "O2"),
     Approach = paste("Bivariate Selection on", select_type),
     Estimate = theta_adj,
     SE       = sqrt(total_var),
     CI_Lower = theta_adj - 1.96 * sqrt(total_var),
-    CI_Upper = theta_adj + 1.96 * sqrt(total_var)
+    CI_Upper = theta_adj + 1.96 * sqrt(total_var),
+    Failed.proportion = sum(!valid) / M
   )
 
     rownames(results_df) <- NULL
@@ -544,7 +616,7 @@ logit <- function(p) log(p / (1 - p))
 
 
 
-impose_orb <- function(data, p1 = 0.4, delta_sim = 0.5, select_type = "zscore", orb.se = FALSE) {
+impose_orb <- function(data, p1 = 0.4, delta_sim = 0.5, select_type = "zscore", orb.se = FALSE, n.total = 100) {
   
   #extract stuff from attributes
   theta1 <- attr(data, "theta1")
@@ -585,6 +657,11 @@ impose_orb <- function(data, p1 = 0.4, delta_sim = 0.5, select_type = "zscore", 
  
   if (orb.se) data_orb$O1_sei[reported_flag == 0] <- NA 
   
+  # add the sample size column
+  data_orb$n.total <- n.total
+
+
+
   return(data_orb)
 }
 
